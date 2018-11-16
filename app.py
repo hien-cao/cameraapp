f63dd1849aa2acdc349f99389c2b024a462925c6
Hien Cao <hien.cao@aalto.fi>
2018-11-16 09:17:11 +0200

    Add camera clone from https://github.com/miguelgrinberg/flask-video-streamin/

--- app.py
+++ app.py
@@ -134,6 +134,21 @@ def logout():
 def dashboard():
   return render_template('dashboard.html')
 
+# Camera setup
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == '__main__':
   app.secret_key = 'secret123'
   app.run(debug = True)
