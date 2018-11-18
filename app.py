52d24db8da9c2ab5103be4f7dfb1dc4d16b2f32a
Hien Cao <hien.cao@aalto.fi>
2018-11-18 22:44:13 +0200

    Add LED and Buzzer and scrolling LCD

--- app.py
+++ app.py
@@ -27,6 +27,20 @@ buzzerPin = 16
 
 app = Flask(__name__)
 
+# init led & buzzer pins
+GPIO.setmode(GPIO.BCM)
+
+pins = {
+   12 : {'name' : 'LED', 'state' : GPIO.LOW},
+   16 : {'name' : 'BUZZER', 'state' : GPIO.LOW}
+   }
+
+# Set each pin as an output and make it low:
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
+
 # Config MySQL
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -39,19 +53,14 @@ mysql.init_app(app)
 
 # Home
 @app.route('/')
+@app.route('/home')
 def home():
   return render_template('home.html')
 
 # About
 @app.route('/about')
 def about():
-  return render_template('about.html')
-
-# Dashboard
-@app.route('/dashboard')
-@is_logged_in
-def dashboard():
-  return render_template('dashboard.html')
+  return render_template('about.html', title = 'About')
 
 # Register Form Class
 class RegisterForm(Form):
@@ -92,7 +101,7 @@ def register():
     
     return redirect(url_for('login'))
   else:
-    error = "it is complete"
+    error = "Error happens!"
   return render_template('register.html', form = form)
 
 # User Login
@@ -151,6 +160,40 @@ def logout():
   flash('You are now logged out', 'success')
   return redirect(url_for('login'))
 
+# Video
+@app.route('/video')
+@is_logged_in
+def video(changePin, action):
+  if request.method == 'POST':
+    staticLine = [session['username'], '',]
+    message = request.form['message']
+    display_message(message, staticLine)
+  
+  # Control LED and BUZZER
+  # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # Get the device name for the pin being changed:
+   deviceName = pins[changePin]['name']
+   # If the action part of the URL is "on," execute the code indented below:
+   if action == "on":
+      # Set the pin high:
+      GPIO.output(changePin, GPIO.HIGH)
+      # Save the status message to be passed into the template:
+      message = "Turned " + deviceName + " on."
+   if action == "off":
+      GPIO.output(changePin, GPIO.LOW)
+      message = "Turned " + deviceName + " off."
+
+   # For each pin, read the pin state and store it in the pins dictionary:
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'pins' : pins
+   }
+  return render_template('video.html', **templateData)
+
 # Camera setup
 def gen(camera):
     """Video streaming generator function."""
@@ -168,11 +211,9 @@ def video_feed():
 
 if __name__ == '__main__':
   try:
-    staticLine = [ 'Hello!','', ]
-    scrollLine = "Warchest"
-    main()
     app.secret_key = 'secret123'
     #app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
     app.run(host='0.0.0.0', debug=True)
+    display_message(['The app is running', '',], 'Welcome to surveillence system 4000', 15)
   except KeyboardInterrupt:
     pass
--- lcd_control.py
+++ lcd_control.py
@@ -17,12 +17,13 @@ def loop_string(string, lcd, framebuffer, row, num_cols, delay=0.2):
     write_to_lcd(lcd, framebuffer, num_cols)
     time.sleep(delay)
 
-def main():
-  start = time.time()
-  period = 15
-
-  while true:
+def display_message(staticLine, scrollLine, period = 0):
+  if period != 0:
+    start = time.time()
+    while True:
+      loop_string(scrollLine, lcd, staticLine, 1, 16)
+      if time.time() > start + period:
+        loop_string("", lcd, "", 1, 16)
+        break
+  else:
     loop_string(scrollLine, lcd, staticLine, 1, 16)
-    if time.time() > start + period:
-      loop_string("", lcd, "", 1, 16)
-      break
