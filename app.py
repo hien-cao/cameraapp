01e7e62abaff1056d557e609892dc6cca7ec40fa
Hien Cao <hien.cao@aalto.fi>
2018-11-18 23:07:49 +0200

    Fix error on LED

--- app.py
+++ app.py
@@ -55,6 +55,14 @@ mysql.init_app(app)
 @app.route('/')
 @app.route('/home')
 def home():
+  # For each pin, read the pin state and store it in the pins dictionary:
+  for pin in pins:
+    pins[pin]['state'] = GPIO.input(pin)
+  # Put the pin dictionary into the template data dictionary:
+  templateData = {
+    'pins' : pins
+    }
+  # Pass the template data into the template main.html and return it to the user
   return render_template('home.html')
 
 # About
@@ -131,7 +139,7 @@ def login():
         session['username'] = username
 
         flash('You are now logged in', 'success')
-        return redirect(url_for('dashboard'))
+        return redirect(url_for('video'))
       else:
         error = "Invalid password"
         return render_template('login.html', error = error)
@@ -171,27 +179,27 @@ def video(changePin, action):
   
   # Control LED and BUZZER
   # Convert the pin from the URL into an integer:
-   changePin = int(changePin)
-   # Get the device name for the pin being changed:
-   deviceName = pins[changePin]['name']
-   # If the action part of the URL is "on," execute the code indented below:
-   if action == "on":
-      # Set the pin high:
-      GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
-   if action == "off":
-      GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
+  changePin = int(changePin)
+  # Get the device name for the pin being changed:
+  deviceName = pins[changePin]['name']
+  # If the action part of the URL is "on," execute the code indented below:
+  if action == "on":
+    # Set the pin high:
+    GPIO.output(changePin, GPIO.HIGH)
+    # Save the status message to be passed into the template:
+    message = "Turned " + deviceName + " on."
+  if action == "off":
+    GPIO.output(changePin, GPIO.LOW)
+    message = "Turned " + deviceName + " off."
 
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
+  # For each pin, read the pin state and store it in the pins dictionary:
+  for pin in pins:
+    pins[pin]['state'] = GPIO.input(pin)
 
-   # Along with the pin dictionary, put the message into the template data dictionary:
-   templateData = {
-      'pins' : pins
-   }
+  # Along with the pin dictionary, put the message into the template data dictionary:
+  templateData = {
+    'pins' : pins
+  }
   return render_template('video.html', **templateData)
 
 # Camera setup
